6ae7447ac5acfdcf037998f043396d6196618f79
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-04-05 20:37:11 +0600

    Fix PUT method to non-existent category

--- routers/categories.ts
+++ routers/categories.ts
@@ -90,6 +90,20 @@ categoriesRouter.delete('/:id', async (req, res, next) => {
 categoriesRouter.put('/:id', async (req, res, next) => {
   try {
     const id = req.params.id;
+
+    const [categories_ids] = await mySqlDb
+      .getConnection()
+      .query('SELECT id FROM categories');
+
+    const result = JSON.stringify(categories_ids);
+    const parsed: Record<'id', number>[] = JSON.parse(result);
+
+    const foundIndex = parsed.findIndex((place) => place.id === Number(id));
+
+    if (foundIndex === -1) {
+      return res.status(404).send({ error: 'Not Found!' });
+    }
+
     if (!req.body.name) {
       return res.status(422).send({ error: 'Category name is required!' });
     }
