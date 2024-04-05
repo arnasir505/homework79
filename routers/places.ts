2bed8b6dc10661ade97470ac778b52f0d48d20c0
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-04-05 20:33:38 +0600

    Add PUT method to placesRouter

--- routers/places.ts
+++ routers/places.ts
@@ -87,4 +87,43 @@ placesRouter.delete('/:id', async (req, res, next) => {
   }
 });
 
+placesRouter.put('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+
+    const [places_ids] = await mySqlDb
+      .getConnection()
+      .query('SELECT id FROM places');
+
+    const result = JSON.stringify(places_ids);
+    const parsed: Record<'id', number>[] = JSON.parse(result);
+
+    const foundIndex = parsed.findIndex((place) => place.id === Number(id));
+
+    if (foundIndex === -1) {
+      return res.status(404).send({ error: 'Not Found!' });
+    }
+
+    if (!req.body.name) {
+      return res.status(422).send({ error: 'Place name is required!' });
+    }
+
+    const placeData: Resource = {
+      name: req.body.name,
+      description: req.body.description || null,
+    };
+
+    await mySqlDb
+      .getConnection()
+      .query('UPDATE places SET name=?, description=? ' + `WHERE id=${id}`, [
+        placeData.name,
+        placeData.description,
+      ]);
+
+    return res.send({ id: id, ...placeData });
+  } catch (error) {
+    next(error);
+  }
+});
+
 export default placesRouter;
